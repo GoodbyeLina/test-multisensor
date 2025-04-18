224d321b2bafeb481fdba601e633da58f977e246
GoodbyeLina <979789689@qq.com>
2025-04-18 11:22:45 +0800

    更新README.md，记录所有改动

---
+++ components/my_mpu6050/my_mpu6050.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <stdio.h>
+#include "unity.h"
+#include "driver/i2c.h"
+#include "mpu6050.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+#define I2C_MASTER_SCL_IO 26      /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO 25      /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUM_0  /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ 100000 /*!< I2C master clock frequency */
+
+static mpu6050_handle_t mpu6050 = NULL;
+
+ /**
+  * @brief i2c master initialization
+  */
+ static void i2c_bus_init(void);
+
+  /**
+  * @brief i2c master initialization
+  */
+ static void i2c_sensor_mpu6050_init(void);
+
